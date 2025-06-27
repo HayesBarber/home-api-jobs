fe2c77e1bede5250bb85897df9d17e1d5877e2c7
Hayes Barber <barberhayes@gmail.com>
2025-06-26 21:39:13 -0400

    auto commit

--- app/jobs/checkForOffline.js
+++ app/jobs/checkForOffline.js
@@ -2,6 +2,7 @@ import { runJob } from "../jobRunner.js";
 
 export function checkForOfflineJob() {
   const JOB_INTERVAL_MS = 10 * 60 * 1000;
+
   setInterval(
     runJob("/discovery/check-offline", "Check offline"),
     JOB_INTERVAL_MS

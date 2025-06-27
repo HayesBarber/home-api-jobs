711e379d05d7a5c83a67531abdade7f49767bf23
Hayes Barber <barberhayes@gmail.com>
2025-06-26 21:35:10 -0400

    use job runner

--- jobs/discoveryJob.js
+++ jobs/discoveryJob.js
@@ -1,15 +1,10 @@
+import { runJob } from "./jobRunner";
+
 export function discoveryJob() {
   const JOB_INTERVAL_MS = 5 * 60 * 1000;
 
-  async function runJob() {
-    try {
-      const response = await fetch("http://localhost:8000/discovery/discover");
-      const data = await response.json();
-      console.log("Discovery response:", data);
-    } catch (error) {
-      console.error("Error fetching discovery:", error);
-    }
-  }
-
-  setInterval(runJob, JOB_INTERVAL_MS);
+  setInterval(
+    runJob("/discovery/discover", "Trigger discovery"),
+    JOB_INTERVAL_MS
+  );
 }

f563b7c6c295a60d5e096395cd6917beb9c6bf5a
William Hayes Barber <barberhayes@gmail.com>
2025-06-26 21:04:38 -0400

    Discovery job (#1)

---
+++ jobs/discoveryJob.js
@@ -0,0 +1,15 @@
+export function discoveryJob() {
+  const JOB_INTERVAL_MS = 1000;
+
+  async function runJob() {
+    try {
+      const response = await fetch("http://localhost:8000/discovery/discover");
+      const data = await response.json();
+      console.log("Discovery response:", data);
+    } catch (error) {
+      console.error("Error fetching discovery:", error);
+    }
+  }
+
+  setInterval(runJob, JOB_INTERVAL_MS);
+}
---
+++ jobs/index.js
@@ -0,0 +1,4 @@
+import { discoveryJob } from "./discoveryJob.js";
+
+console.log("Starting jobs...");
+discoveryJob();

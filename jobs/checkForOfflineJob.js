e9a455099d8ac2e264e5b4cc801bf5b1827664de
Hayes Barber <barberhayes@gmail.com>
2025-06-26 21:33:12 -0400

    create job runner

--- jobs/checkForOfflineJob.js
+++ jobs/checkForOfflineJob.js
@@ -1,17 +1,9 @@
+import { runJob } from "./jobRunner.js";
+
 export function checkForOfflineJob() {
   const JOB_INTERVAL_MS = 10 * 60 * 1000;
-
-  async function runJob() {
-    try {
-      const response = await fetch(
-        "http://localhost:8000/discovery/check-offline"
-      );
-      const data = await response.json();
-      console.log("Check offline response:", data);
-    } catch (error) {
-      console.error("Error fetching check-offline:", error);
-    }
-  }
-
-  setInterval(runJob, JOB_INTERVAL_MS);
+  setInterval(
+    runJob("/discovery/check-offline", "Check offline"),
+    JOB_INTERVAL_MS
+  );
 }

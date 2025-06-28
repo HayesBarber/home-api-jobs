060d045584f2913d7c6a91cd6358ac171f042be4
Hayes Barber <barberhayes@gmail.com>
2025-06-27 20:11:53 -0400

    adjust job runner

--- app/util/jobRunner.js
+++ app/util/jobRunner.js
@@ -1,11 +1,18 @@
+/**
+ * @param {() => Promise<import("../../generated/Api").HttpResponse>} jobFn
+ * @param {string} label
+ */
 export function runJob(jobFn, label) {
   return async () => {
     try {
       console.log(`Running job: ${label}`);
-      await jobFn();
-      console.log(`Completed job: ${label}`);
+      const result = await jobFn();
+      const json = await result.json();
+      const code = result.status;
+      console.log("Completed job", label);
+      console.log("Status code:", code, "Json:", json);
     } catch (err) {
-      console.error(`Job failed (${label}):`, err);
+      console.error(label, "Job Failed:", err);
     }
   };
 }
